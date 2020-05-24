31c4c389bd1d344a0ffe94a52fdddd2d44122b3d
Rafael Gonçalves <rafael@pop-os.localdomain>
2020-05-24 14:25:53 +0300

    add routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,8 +3,18 @@ const bodyParser = require('body-parser');
 const cors = require("cors");
 const app = express();
 
+const db = require("./util/dbconection.ts");
+
 //Define a porta do backend
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3005;
+
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 // cors
 app.use(cors()) //cors não tem nenhum filtro
@@ -19,6 +29,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.json({ message: "Bem-vindo ao login" });
 });
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
 
 app.listen(PORT, () => {
   console.log(`Server esta rodando na porta: ${PORT}.`);
