559e0828e371a3dffb41c72688e1ed476e77c168
Rafael Goncalves <llrafaell@gmail.com>
2020-05-25 18:12:57 +0300

    Add JWT

---
+++ src/migration/1590387757149-CreateAdminUser.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner, getRepository } from "typeorm";
+import { User } from "../entity/User";
+
+
+export class CreateAdminUser1590387757149 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        let user = new User();
+        user.username = "admin";
+        user.password = "admin";
+        user.hashPassword();
+        user.role = "ADMIN";
+        const userRepository = getRepository(User);
+        await userRepository.save(user);
+      }
+    
+      public async down(queryRunner: QueryRunner): Promise<any> {}
+
+}
